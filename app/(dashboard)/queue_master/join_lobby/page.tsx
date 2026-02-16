4aca68f939dbf43475b4939db21b1fdb1a582144
unknown <joshuafrellt@gmail.com>
2026-02-16 19:18:43 +0800

    feat: join lobby

--- app/(dashboard)/queue_master/join_lobby/page.tsx
+++ app/(dashboard)/queue_master/join_lobby/page.tsx
@@ -1,7 +1,38 @@
+"use client"
+
+import JoinTile from "@/app/components/ui/JoinTile";
+import { MdQrCodeScanner } from "react-icons/md";
+import { FaLink } from "react-icons/fa6";
+import AuthFormInput from "@/app/components/ui/AuthFormInput";
+import { useState } from "react";
 
 export default function JoinLobbiesPage() {
+    const [joinUrl, setJoinUrl] = useState("")
 
     return (
-        <h1>JOIN LOBBIES PAGE</h1>
+        <div className="flex flex-col lg:flex-row lg:justify-center items-center gap-16 lg:gap-32 mt-8 lg:mt-42">
+            <JoinTile>
+                <div className="flex flex-col items-center gap-4">
+                    <div className="flex gap-2">
+                        <p className="font-bold text-2xl flex items-center gap-2">Join via Link</p>
+                        <FaLink className="text-3xl"/>
+                    </div>
+                    <AuthFormInput 
+                        type="url"
+                        placeholder="Link"
+                        value={joinUrl}
+                        onChange={(e) => setJoinUrl(e.target.value)}
+                    />
+                    <button className="bg-secondary px-5 py-2 font-semibold rounded-sm border-2 border-accent cursor-pointer transform duration-100 hover:rounded-xl">Confirm</button>
+                </div>
+            </JoinTile>
+            <JoinTile>
+                <div className="flex flex-col items-center cursor-pointer">
+                    <p className="font-bold text-2xl flex items-center gap-2">Join via QR</p>
+                    <MdQrCodeScanner className="text-9xl"/>
+                </div>
+            </JoinTile>
+        </div>
+        
     )
 }
---
+++ app/components/ui/JoinTile.tsx
@@ -0,0 +1,13 @@
+
+interface joinTileProps {
+    children: React.ReactNode
+}
+
+export default function JoinTile({ children }: joinTileProps) {
+
+    return (
+        <div className="bg-secondary w-80 aspect-square flex justify-center items-center rounded-xl border-3 border-accent shadow-lg">
+            {children}
+        </div>
+    )
+}
